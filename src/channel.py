92459abb1b92e6424760f4b224b2c774f60fdbcb
Galina Pudovkina <galinapudovkina8@mail.ru>
2023-06-29 15:55:49 +0300

    add magical methods

--- src/channel.py
+++ src/channel.py
@@ -15,10 +15,44 @@ class Channel:
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.description = self.channel["items"][0]["snippet"]["description"]
-        self.url = f"https://www.youtube.com/channel/%7Bself.channel_id%7D"
+        self.url = f"https://www.youtube.com/channel/" + self.channel['items'][0]['id']  # %7Bself.channel_id%7D"
         self.subscriber_count = int(self.channel["items"][0]["statistics"]["subscriberCount"])
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
         self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
+
+    def __str__(self):
+        """Магический метод, передставляющий инфо о классе пользователю.
+           Выводит название канала и ссылку на него
+        """
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        """Магиский метод для сложения кол-ва подписчиков разных каналов"""
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        """Магиский метод для вычитания кол-ва подписчиков разных каналов"""
+        return self.subscriber_count - other.subscriber_count
+
+    def __gt__(self, other):
+        """Магиский метод для сравнения кол-ва подписчиков разных каналов"""
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        """Магиский метод для сравнения кол-ва подписчиков разных каналов"""
+        return self.subscriber_count >= other.subscriber_count
+
+    def __lt__(self, other):
+        """Магиский метод для сравнения кол-ва подписчиков разных каналов"""
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        """Магиский метод для сравнения кол-ва подписчиков разных каналов"""
+        return self.subscriber_count <= other.subscriber_count
+
+    def __eq__(self, other):
+        """Магиский метод для сравнения кол-ва подписчиков разных каналов"""
+        return self.subscriber_count == other.subscriber_count
 
     @property
     def channel_id(self):
