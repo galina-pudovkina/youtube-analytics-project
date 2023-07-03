71d8a3abf4a95d491f53a853c9a7c3c9535c1391
Galina Pudovkina <galinapudovkina8@mail.ru>
2023-07-03 19:49:53 +0300

    added classes Video and PLVideo

--- src/channel.py
+++ src/channel.py
@@ -1,7 +1,6 @@
 import googleapiclient.discovery
 import os
 import json
-from functools import total_ordering
 
 
 class Channel:
@@ -15,7 +14,7 @@ class Channel:
         self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         self.title = self.channel["items"][0]["snippet"]["title"]
         self.description = self.channel["items"][0]["snippet"]["description"]
-        self.url = f"https://www.youtube.com/channel/" + self.channel['items'][0]['id']  # %7Bself.channel_id%7D"
+        self.url = f"https://www.youtube.com/channel/" + self.channel['items'][0]['id']
         self.subscriber_count = int(self.channel["items"][0]["statistics"]["subscriberCount"])
         self.video_count = self.channel["items"][0]["statistics"]["videoCount"]
         self.view_count = self.channel["items"][0]["statistics"]["viewCount"]
---
+++ src/video.py
@@ -0,0 +1,56 @@
+import googleapiclient.discovery
+import os
+
+
+class Video:
+    """Класс для представления видео"""
+
+    api_key: str = os.getenv('YOUTUBE_API_KEY')
+
+    def __init__(self, video_id):
+        self.youtube = self.get_service()
+        self.video = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                id=video_id
+                                                ).execute()
+
+        self.video_id = video_id
+        self.title = self.video["items"][0]["snippet"]["title"]
+        self.url = f"https://www.youtube.com/video/" + self.video['items'][0]['id']
+        self.view_count = self.video["items"][0]["statistics"]["viewCount"]
+        self.likes = self.video['items'][0]['statistics']['likeCount']
+
+    @classmethod
+    def get_service(cls):
+        """Класс-метод, возвращающий объект для работы с YouTube API"""
+        return googleapiclient.discovery.build('youtube', 'v3', developerKey=cls.api_key)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class PLVideo(Video):
+    """Дочерний класс для представления видео из плейлиста"""
+
+    api_key: str = os.getenv('YOUTUBE_API_KEY')
+
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+        self.youtube = self.get_service()
+        self.playlist = self.youtube.playlistItems().list(playlistId=playlist_id,
+                                                          part='contentDetails',
+                                                          maxResults=50,
+                                                          ).execute()
+
+        self.title = self.video["items"][0]["snippet"]["title"]
+        self.url = f"https://www.youtube.com/video/" + self.video['items'][0]['id']
+        self.view_count = self.video["items"][0]["statistics"]["viewCount"]
+        self.likes = self.video['items'][0]['statistics']['likeCount']
+
+    @classmethod
+    def get_service(cls):
+        """Класс-метод, возвращающий объект для работы с YouTube API"""
+        return googleapiclient.discovery.build('youtube', 'v3', developerKey=cls.api_key)
+
+    def __str__(self):
+        return f'{self.title}'
